b082ac38150e6f2c122f4f0a96b6d55810eb63df
Mauro Nunes <maurao.dev@gmail.com>
2017-03-12 19:03:58 -0300

    Criação de serviços

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,17 +1,16 @@
 angular.module('alurapic')
-    .controller('FotoController', function ($scope, $http, $routeParams) {
+    .controller('FotoController', function ($scope, recursoFoto, $routeParams) {
 
         $scope.foto = {};
         $scope.mensagem = '';
 
         if ($routeParams.fotoId) {
-            $http.get('/v1/fotos/' + $routeParams.fotoId)
-                .success(function (retorno) {
-                    $scope.foto = retorno;
-                })
-                .error(function (error) {
-                    $scope.messagem = 'Não foi possível encontrar a foto';
-                });
+            recursoFoto.get({fotoId: $routeParams.fotoId}, function(foto) {
+                $scope.foto = foto; 
+            }, function(erro) {
+                console.log(erro);
+                $scope.mensagem = 'Não foi possível obter a foto'
+            });
         }
 
         $scope.submeter = function () {
@@ -19,22 +18,19 @@ angular.module('alurapic')
             if ($scope.formulario.$valid) {
 
                 if ($routeParams.fotoId) {
-                    $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
-                        .success(function () {
-                            $scope.mensagem = 'Foto ' + $scope.foto.titulo + ' foi alterada';
-                        })
-                        .error(function (erro) {
-                            console.log(erro);
-                            $scope.mensagem = 'Não foi possível alterar';
-                        });
+                    recursoFoto.update({fotoId: $scope.foto._id},
+                    $scope.foto = function() {
+                        $scope.mensagem = 'Foto alterada com sucesso';
+                    }, function(erro) {
+                        console.log(erro);
+                        $scopre.mensagem = 'Não foi possível alterar a foto';
+                    });
 
                 } else {
-                    $http.post('/v1/fotos', $scope.foto)
-                        .success(function () {
+                    recursoFoto.save($scope.foto, function() {
                             $scope.foto = {};
                             $scope.mensagem = 'Foto cadastrada com sucesso';
-                        })
-                        .error(function (erro) {
+                        }, function (erro) {
                             console.log(erro);
                             $scope.mensagem = 'Foto cadastrada com sucesso';
                         });
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,26 +1,22 @@
-angular.module('alurapic').controller('FotosController', function ($scope, $http) {
+angular.module('alurapic').controller('FotosController', function ($scope, recursoFoto) {
 
     $scope.fotos = [];
     $scope.filtros = '';
 
-    $http.get('/v1/fotos')
-    .success(function(retorno) {
-        $scope.fotos = retorno;
-    })
-    .error(function(erro) {
+    recursoFoto.query(function(fotos) {
+        $scope.fotos = fotos;
+    }, function(erro) {
         console.log(erro);
     });
 
     $scope.remover = function(foto) {
-        $http.delete('/v1/fotos/' + foto._id)
-        .success(function() {
+        recursoFoto.delete({fotoId: foto._id}, function() {
             var indiceDaFoto = $scope.fotos.indexOf(foto);
             $scope.fotos.splice(indiceDaFoto, 1);
-            $scope.mensagem = "Foto " + foto.titulo + ' removida com sucesso!';
-        })
-        .error(function(erro) {
-            console.dir(erro);
-            $scopre.mesagem = 'Não foi possível remover a foto' + foto.titulo;
+            $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
+        }, function(erro) {
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
         });
     };
 
